f8678cc40d7affb37c0ff49a0f48aeedc11196fa
Army R-code <jarupa310@gmail.com>
2021-06-02 20:08:31 -0500

    Ejemplos de funciones

--- sesion_2/reto_3/reto3_funcion.js
+++ sesion_2/reto_3/reto3_funcion.js
@@ -7,3 +7,28 @@ function logName() {
     console.log(name);
 }
 logName();
+
+// Funciones
+function addition(a, b) {
+return a + b 
+}
+
+var result = addition(8, 5);
+console.log(result)
+
+// expresion de funciones: funcion anónima
+
+var subtraction = function(c, d) {
+    return c - d
+}
+
+var result = subtraction(4, 2);
+console.log(result);
+
+//A veces es necesario que la expresión de la función no sea anónima. Por ejemplo, en una función recursiva donde es necesario hacer referencia a sí misma.
+
+var factorial = function fac(number) {
+    return /*condicion*/ number < 1 ? /*si es verdad*/ 1 : /*de otro modo*/ number * fac(number -1)
+}
+
+console.log(factorial(5));
